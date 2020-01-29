262a90aa5ef154043fa453be7a2d14bce3a89ed6
Michael Napoli <napoli.38@osu.edu>
2020-01-29 17:38:28 -0500

    new lecture

---
+++ cse2122/lectures/class_29_01_2020.cpp
@@ -0,0 +1,31 @@
+// Class: CSE 2122 Lecture 6
+// Date: 1/24/2020
+// topics include: class types
+/*
+notes:
+   - classes are user-defined data types
+   - classes can include:
+      - member variables
+      - member functions
+      - and require descriptions
+   - class member functions must be initialized similarly to regular program functions
+   - Example: 
+      return_type Class_name::function_name (intput);
+   - the '::' operator is important as it is the scope resolution
+      - tells the class that the function is a member function
+      - the Class_name which precedes '::' is a type qualifier
+   - to call the member function from the program code use dot operator syntax
+   - Example:
+      member function: output();
+      in code: variable_name.output();
+   - the 'private' portion of a class can only be accessed from the class type and member functions inside the class
+   - the 'public' portion of a class can be accessed by the main program as well as within the class 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
