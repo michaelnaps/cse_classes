d5383fbcd24e8b42b08ef2ea94899746bc252e5e
michaelnaps <napoli.38@osu.edu>
2021-05-21 11:32:58 -0400

    hw2 started

---
+++ cse4256/week2/napoli_38_hw2.py
@@ -0,0 +1,11 @@
+# File: napoli_38_hw2.py
+# Created by: Michael Napoli
+# Created on: 5/21/2021
+
+# import necessary directories
+
+# Problem 1
+# create test list
+olist = ['test', 'test1', 'omitted', 'pie', 'michael', 'napoli', 'ava', 'jane']
+nlist = [word for word in olist if len(word) <= 4]
+print(nlist)
