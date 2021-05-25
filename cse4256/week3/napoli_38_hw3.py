c2acce80de5d775e32b3dd15dabeee16a3a892fb
michaelnaps <napoli.38@osu.edu>
2021-05-24 21:07:16 -0400

    problem 2 and 3 complete

--- cse4256/week3/napoli_38_hw3.py
+++ cse4256/week3/napoli_38_hw3.py
@@ -2,14 +2,27 @@
 # Created by: Michael Napoli
 # Created on: 5/24/2021
 
+
 # Problem 1
-def unique_ints(n):
-    # if the length of the set is equat to the array, all unique integers
+# determine if given integer array has all unique values
+# NOTE: assumption that list is made up of integers, but any type
+#       list should also work
+def unique_list(n):
+    # if the length of the set is equat to the array, unique list
     return len(n) == len(set(n))
+
+# test cases
+# print(unique_list(range(10)))  # unique list of integers
+# print(unique_list([1, 2, 2]))  # non-unique list of integers
 
 
 # Problem 2
+# determine if given string contains all vowels, return bool
 def vowel_search(s):
-    str_list = s.lower()
-    vow_list = [word for word in s if chr not in "aeiou"]
-    return len(str_list) == len(vow_list)
+    s = set(s.lower())  # create set of lower characters
+    # check for each vowel in the given string
+    return ('a' in s) and ('e' in s) and ('i' in s) and ('o' in s) and ('u' in s)
+
+# test cases
+# print(vowel_search("All vowels with unicorns"))  # all vowels are present
+# print(vowel_search("I needed as many letters"))  # not all vowels present
