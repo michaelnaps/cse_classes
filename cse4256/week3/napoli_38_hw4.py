18c1cecdff234d8436348cc88891870f0fc235b6
michaelnaps <napoli.38@osu.edu>
2021-06-01 13:00:41 -0400

    finished problem 1, no dict., suppressed output

--- cse4256/week3/napoli_38_hw4.py
+++ cse4256/week3/napoli_38_hw4.py
@@ -8,19 +8,26 @@ import string
 # Problem 1
 # given a sentence, find the most common letter
 def most_common_letter(s):
-    max = 0
-    c = s.lower()
-    n = numpy.zeros(26)
-    alph = 'abcdefghijklmnopqrstuvwxyz'
+    max = 0                                 # max element location
+    c = s.lower()                           # convert sentence to lower case
+    n = numpy.zeros(26)                     # array for counts of each letter
+    alph = 'abcdefghijklmnopqrstuvwxyz'     # alphabet array for search
 
+    # loop for letter count
     for i in range(0, len(alph)):
-        if alph[i] in c:
-            n[i] += 1
-        if n[max] <= n[i]:
+        for j in range(0, len(c)):
+            if alph[i] == c[j]:
+                n[i] += 1
+
+        # check if the current letter is more common than current max
+        if n[max] < n[i]:
             max = i
 
     return alph[max]
 
 # test cases
-print(most_common_letter('AAAaa NNNNNNNNNN,ppPPPp Hht'))
-print(most_common_letter('My name is Michael Napoli!!'))
+# print(most_common_letter('AAAaa NNNNNNNNNN,ppPPPp Hht'))
+# print(most_common_letter('My name is Michael Napoli!!'))
+
+
+# Problem 2
