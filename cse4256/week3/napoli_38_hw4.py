be79a11ca7bbe192044715762725cde8caa60a9e
michaelnaps <napoli.38@osu.edu>
2021-06-03 19:27:38 -0400

    problem 1-4 complete, problem 1 couold be converted to use dictionary variables

--- cse4256/week3/napoli_38_hw4.py
+++ cse4256/week3/napoli_38_hw4.py
@@ -3,7 +3,6 @@
 # Due Date: 6/6/2021
 
 import numpy
-import string
 
 # Problem 1
 # given a sentence, find the most common letter
@@ -12,8 +11,6 @@ def most_common_letter(s):
     c = s.lower()                           # convert sentence to lower case
     n = numpy.zeros(26)                     # array for counts of each letter
     alph = 'abcdefghijklmnopqrstuvwxyz'     # alphabet array for search
-
-    alph_n = {}
 
     # loop for letter count
     for i in range(0, len(alph)):
@@ -25,7 +22,7 @@ def most_common_letter(s):
         if n[max] < n[i]:
             max = i
 
-    return alph[max]
+    return alph[max]  # return most common letter found
 
 # test cases
 # print(most_common_letter('AAAaa NNNNNNNNNN,ppPPPp Hht'))
@@ -34,9 +31,61 @@ def most_common_letter(s):
 
 # Problem 2
 # create dictionary variable from two string variables
-def create_dict(index, items):
-    # check that lists are of equal length
-    if (len(index) != len(items)):
+def create_dict(index, item):
+    # check that lists are of equal length, if not return -1
+    if (len(index) != len(item)):
         return -1
 
+    d = {}  # initialize empty dictionary variable
+    for i in range(0, len(index)):
+        # create appropriately indexed dictionary
+        d[index[i]] = item[i]
+
     return d
+
+
+# test cases
+# print(create_dict(['cat'], ['egg']))
+# print(create_dict(['cat', 'dog', 'parrot'], ['egg', 'bacon', 'toast']))
+
+
+# Problem 3
+# count number of iterations for given value with bisection method
+def bisection_count(n):
+    c = 1    # initialize count variable to 0
+    g = 50   # initialize guess to 50 (always)
+    l = 0    # initialize range variables to 1 and 100
+    h = 101
+
+
+    # use bisection method to solve for user's guess
+    while (g != n):  # infinite loop
+        # adjust range based on current guess
+        if g < n:
+            l = g
+        elif g > n:
+            h = g
+
+        g = int((l + h) / 2)  # divide range by two for next guess and truncate
+        c += 1  # iterate guess counter
+
+    return c
+
+# test cases for bisection_count() function
+# print(bisection_count(50))
+# print(bisection_count(25))
+# print(bisection_count(12))
+
+
+# Problem 4
+# create dictionary variable of number of guesses from bisection method (max 7)
+def bisection_dict():
+    # initialize number of guess dictionary variable
+    d = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0}
+    for i in range(1, 101):
+        d[bisection_count(i)] += 1
+
+    return d
+
+# test
+# print(bisection_dict())
