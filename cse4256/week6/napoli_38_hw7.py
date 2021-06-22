47f38c987efa0e9f65f079e58abdddebfb4dd20c
michaelnaps <napoli.38@osu.edu>
2021-06-22 11:02:21 -0400

    completed problem 2

--- cse4256/week6/napoli_38_hw7.py
+++ cse4256/week6/napoli_38_hw7.py
@@ -21,6 +21,49 @@ mat_1 = [
     [1, 0, 0, 1],
     [0, 0, 0, 1],
     [0, 1, 1, 0]
-    ]
+]
 
-print(complementw2(mat_1))
+# print(complementw2(mat_1))
+
+
+# Problem 2
+def complement2(d):
+    k = list(d.keys())
+    cd = {k[i]: None for i in range(0, len(k))}
+
+    for i in range(0, len(d)):
+        for j in range(0, len(k)):
+            if k[j] not in d[k[i]] and i != j:
+                if cd[k[i]] == None:
+                    cd[k[i]] = [k[j]]
+                else:
+                    cd[k[i]].append(k[j])
+
+    return cd
+
+# test cases
+d_1 = {
+    0: [1, 2, 4],
+    1: [0, 2, 4],
+    2: [0, 1, 3],
+    3: [2, 4],
+    4: [1, 3]
+}
+
+print(complement2(d_1))
+
+
+# Problem 3
+def transpose1(m):
+    tm = [[0 for j in range(0, len(m))] for i in range(0, len(m))]
+    return tm
+
+# test cases
+mat_2 = [
+    [0, 1, 0, 1],
+    [0, 0, 1, 0],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+]
+
+print(transpose1(mat_2))
