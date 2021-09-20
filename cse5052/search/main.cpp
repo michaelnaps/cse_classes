6df926da465c578eb9ca739111b443429679411b
michaelnaps <napoli.38@osu.edu>
2021-09-20 12:21:14 -0400

    created class folder for AI (cse5052)

---
+++ cse5052/search/main.cpp
@@ -0,0 +1,13 @@
+// Project: Search Algorithm
+// Created by: Michael Napoli
+// Created on: 9/20/2021
+// Last modifies on: 9/20/2021
+
+/* Purpose: For the testing of various search algorithms
+ *     of both the uninformed and informed architecture.
+*/
+
+int main()
+{
+  return 0;
+}
