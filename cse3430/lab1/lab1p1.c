b53bad1856619613c3e1a9745134e720d169c589
michaelnaps <napoli.38@osu.edu>
2021-10-23 16:49:47 -0400

    created initial prompt function

--- cse3430/lab1/lab1p1.c
+++ cse3430/lab1/lab1p1.c
@@ -1,4 +1,22 @@
 // File: lab1_napoli38.c
 // Created by: Michael Napoli
 // Created on: 10/23/2021
-// Due Date:
+// Due Date: 11/1/2021
+
+#include <stdio.h>
+
+// FUNCTION DECLARATIONS
+void prompt_user();
+
+int main()
+{
+  float dataSet[8];
+  const int dataSetSize = 8;
+
+  prompt_user();
+  return 0;
+}
+
+void prompt_user() {
+  printf("Enter data for calculation:\n");
+}
