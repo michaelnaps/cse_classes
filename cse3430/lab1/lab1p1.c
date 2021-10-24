25fdc47525ea4651fca0ba0be94f4bd7c2755714
michaelnaps <napoli.38@osu.edu>
2021-10-24 12:46:23 -0400

    variable type 'float' being referenced as type 'double' in printf

--- cse3430/lab1/lab1p1.c
+++ cse3430/lab1/lab1p1.c
@@ -6,17 +6,41 @@
 #include <stdio.h>
 
 // FUNCTION DECLARATIONS
-void prompt_user();
+void print_data(const float* data, const int n);
+void prompt_user(float* data, const int n);
 
 int main()
 {
   float dataSet[8];
   const int dataSetSize = 8;
 
-  prompt_user();
+  prompt_user(dataSet, dataSetSize);
+
+  print_data(dataSet, dataSetSize);
+
   return 0;
 }
 
-void prompt_user() {
+void print_data(const float* data, const int n) {
+  int i(0);
+
+  for (i = 0; i < n; ++i)
+  { printf("%.3f, ", (float)data[i]); }
+
+  printf("/n");
+
+  return;  // return nothing
+}
+
+void prompt_user(float* data, const int n) {
+  int i(0);
+
   printf("Enter data for calculation:\n");
+
+  // get data from user
+  for (i = 0; i < n; ++i) {
+    scanf("%f", data[i]);
+  }
+
+  return;  // return nothing
 }
