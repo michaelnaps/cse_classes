cbd77293c58d2378768da75ffc49765dec87512d
michaelnaps <napoli.38@osu.edu>
2021-10-25 09:44:48 -0400

    part 1 needs formatting, testing, and comments

--- cse3430/lab1/lab1p1.c
+++ cse3430/lab1/lab1p1.c
@@ -12,8 +12,8 @@ void enter_loop(const float* data, const int n);
 
 float maximum(const float* data, const int n);
 float minimum(const float* data, const int n);
-float sum(const float* data, const int n);
-float average(const float* data, const int n);
+float sum_data(const float* data, const int n);
+float average_data(const float* data, const int n);
 
 
 
@@ -22,10 +22,7 @@ int main() {
   const int dataSetSize = 8;
 
   prompt_user(dataSet, dataSetSize);
-
   enter_loop(dataSet, dataSetSize);
-
-  print_data(dataSet, dataSetSize);
 
   return 0;
 }
@@ -43,7 +40,7 @@ void print_data(const float* data, const int n) {
     }
   }
 
-  printf("\n");
+  printf("\n\n");
 
   return;  /* return nothing */
 }
@@ -62,7 +59,7 @@ void prompt_user(float* data, const int n) {
 }
 
 void enter_loop(const float* data, const int n) {
-  int user_input = 6;
+  int user_input = -1;
 
   while (user_input != 6) {
     printf("1). Find minimum value.\n");
@@ -70,21 +67,23 @@ void enter_loop(const float* data, const int n) {
     printf("3). Calculate the sum of all values.\n");
     printf("4). Calculate the average of all the values.\n");
     printf("5). Print the values in the data set.\n");
-    printf("6). Exit the program.\n\n");
-    printf("Choose computation to perform from list above: ");
+    printf("6). Exit the program.\n");
+    printf("Choose computation to perform from list above:\n");
 
     scanf("%i", &user_input);
 
     if (user_input == 1)
-      minimum(data, n);
+      printf("The minimum for the data is %f.\n\n", minimum(data, n));
     else if (user_input == 2)
-      maximum(data, n);
+      printf("The maximum for the data is %f.\n\n", maximum(data, n));
     else if (user_input == 3)
-      sum(data, n);
+      printf("The sum for the data is %f.\n\n", sum_data(data, n));
     else if (user_input == 4)
-      average(data, n);
-    else if (user_input == 5)
+      printf("The average for the data is %f.\n\n", average_data(data, n));
+    else if (user_input == 5) {
+      printf("The data entered by the user was...\n");
       print_data(data, n);
+    }
     else
       user_input = 6;
   }
@@ -106,13 +105,30 @@ float maximum(const float* data, const int n) {
 }
 
 float minimum(const float* data, const int n) {
-  return 1.0;
+  int i = 1;
+  float min = data[0];
+
+  for (i; i < n; ++i) {
+    if (min > data[i]) {
+      min = data[i];
+    }
+  }
+
+  return min;
 }
 
-float sum(const float* data, const int n) {
-  return 1.0;
+float sum_data(const float* data, const int n) {
+  int i = 0;
+  float sum = 0;
+
+  for (i; i < n; ++i) {
+    sum += data[i];
+  }
+
+  return sum;
 }
 
-float average(const float* data, const int n) {
-  return 1.0;
+float average_data(const float* data, const int n) {
+  float sum = sum_data(data, n);
+  return sum / n;
 }
